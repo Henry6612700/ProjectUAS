a502be59508724c9d41a92f8ce81fc39f04cf7dc
alfidh02 <alfidarmawan79@gmail.com>
2020-01-17 19:07:36 +0700

    Layout changes

--- MyApplication1/app/src/main/java/com/example/myapplication1/SplashScreen.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/SplashScreen.java
@@ -1,8 +1,12 @@
 package com.example.myapplication1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +19,8 @@ public class SplashScreen extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash_screen);
 
+        statusBarTranslucent(this);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -26,5 +32,15 @@ public class SplashScreen extends AppCompatActivity {
 
             }
         }, waktu_loading);
+    }
+    //        Untuk transparan dengan latar belakang
+    public static void statusBarTranslucent(Activity activity){
+        Window window = activity.getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//        hide status bar
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
     }
 }
