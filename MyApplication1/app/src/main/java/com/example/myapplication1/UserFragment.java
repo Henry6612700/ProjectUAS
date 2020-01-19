5f9cbce5cbb2b12bc5e6629f9bddd43a15b597b6
alfidh02 <alfidarmawan79@gmail.com>
2020-01-19 22:37:39 +0700

    Few changes on user and detail fragment

--- MyApplication1/app/src/main/java/com/example/myapplication1/UserFragment.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/UserFragment.java
@@ -40,7 +40,7 @@ public class UserFragment extends Fragment {
     private AppCompatActivity appCompatActivity;
 //    Initiate context
     private Context mContext;
-    private TextView mEmail,mNama;
+    private TextView mEmail,mNama,mKredit;
     private ImageView iProfile;
     private Button btnLogout;
 
@@ -62,15 +62,21 @@ public class UserFragment extends Fragment {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         mEmail = view.findViewById(R.id.email);
         mNama = view.findViewById(R.id.nama);
+        mKredit = view.findViewById(R.id.kredit);
         iProfile = view.findViewById(R.id.imageProfile);
         btnLogout = view.findViewById(R.id.btn_sign_out);
         appCompatActivity = ((AppCompatActivity)getActivity());
 
-//        String photo = String.valueOf(user.getPhotoUrl());
-
         mEmail.setText(user.getEmail());
         mNama.setText(user.getDisplayName());
-//        Picasso.get().load(photo).into(iProfile);
+
+        if (user.getPhotoUrl()!=null) {
+            String photo = String.valueOf(user.getPhotoUrl());
+            Picasso.get().load(photo).into(iProfile);
+            mKredit.setVisibility(View.INVISIBLE);
+        } else {
+          mKredit.setVisibility(View.VISIBLE);
+        }
 
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
